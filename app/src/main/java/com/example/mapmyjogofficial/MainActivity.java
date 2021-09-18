764496d4b49f1cb98d81dccc8e60a66c84c91bb0
Marmik <marmikp19@gmail.com>
2021-09-18 15:12:46 -0600

    feat: UI completed and functional buttons added
    Fully implemented UI for settings page, including additional of functional buttons

--- app/src/main/java/com/example/mapmyjogofficial/MainActivity.java
+++ app/src/main/java/com/example/mapmyjogofficial/MainActivity.java
@@ -26,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.Settings);
 
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+
+
+        /*
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+         */
     }
 
     @Override
@@ -61,10 +66,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
