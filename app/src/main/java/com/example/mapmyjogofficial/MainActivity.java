5fec847fc2223749d166866898ea4fede7e5fb20
Marmik <marmikp19@gmail.com>
2021-09-18 11:28:50 -0600

    feat: preference_version and UI header
    Implemented basic UI for the header and integrated a new preference

--- app/src/main/java/com/example/mapmyjogofficial/MainActivity.java
+++ app/src/main/java/com/example/mapmyjogofficial/MainActivity.java
@@ -16,16 +16,15 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.mapmyjogofficial.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        setSupportActionBar(binding.Settings);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
