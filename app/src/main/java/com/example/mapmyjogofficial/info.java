ce953332be4abe8e0119eaa996f2a54e0db18e65
Yihoi Jung <yihoi0311@gmail.com>
2021-09-18 17:30:01 -0400

    feat: UI for statistics & info pages

---
+++ app/src/main/java/com/example/mapmyjogofficial/info.java
@@ -0,0 +1,13 @@
+package com.example.mapmyjogofficial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
