5f4d833b8998fdd10c8bd43c2cb097dfff7fb83c
Serdar Hasan <63472762+shasan033@users.noreply.github.com>
2021-09-18 18:20:54 -0700

    feat: Added device location and basic UI for routeplanning

--- app/src/main/java/com/example/mapmyjogofficial/FirstFragment.java
+++ app/src/main/java/com/example/mapmyjogofficial/FirstFragment.java
@@ -35,6 +35,14 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_routeplanning);
+            }
+        });
     }
 
     @Override
